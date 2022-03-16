2ccbc42ca7d8a16505db25c8475ff02b7a270db9
Connor Cates <35548067+ccates83@users.noreply.github.com>
2022-03-15 23:23:37 -0400

    Sources folder already excludes stuff

--- Package.swift
+++ Package.swift
@@ -21,11 +21,9 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "CCAppCore",
-            dependencies: [],
-            exclude: ["Sampler"]),
+            dependencies: []),
         .testTarget(
             name: "CCAppCoreTests",
-            dependencies: ["CCAppCore"],
-            exclude: ["Sampler"]),
+            dependencies: ["CCAppCore"]),
     ]
 )
