f2570a925fb01d63e12248625c1af4c6d2f8df72
Connor Cates <35548067+ccates83@users.noreply.github.com>
2022-03-15 23:55:06 -0400

    common button init public

--- Sources/CCAppCore/CommonComponents/CommonButton.swift
+++ Sources/CCAppCore/CommonComponents/CommonButton.swift
@@ -67,7 +67,7 @@ public struct CommonButton: View {
     // It would be nice to make this into a binding.
     private let disabled: Bool
     
-    init(title: String,
+    public init(title: String,
          disabled: Bool = false,
          backgroundColor: Color = Color.green,
          foregroundColor: Color = Color.white,
